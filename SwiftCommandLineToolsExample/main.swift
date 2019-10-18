01a6ba7215561246aa89ee3d0481ca7a4c1a70bc
Pritesh Patel <pritesh.patel@adesa.com>
2019-10-18 09:45:36 -0400

    escape closure example added

--- SwiftCommandLineToolsExample/main.swift
+++ SwiftCommandLineToolsExample/main.swift
@@ -362,3 +362,24 @@ let date = Date(timeIntervalSince1970: unixTimestamp)
 print(date)
 
 print("100".integerValue!)
+
+
+
+func getSumOf(array:[Int], handler: ((Int)->Void)) {
+    //step 2
+    var sum: Int = 0
+    for value in array {
+        sum += value
+    }
+    
+    //step 3
+    handler(sum)
+}
+
+func doSomething() {
+    //setp 1
+    getSumOf(array: [16,756,442,6,23]) {(sum) in
+        print(sum)
+        //step 4, finishing the execution
+    }
+}//It will print the sumof all the given numbers.
